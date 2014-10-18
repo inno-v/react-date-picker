61021a15ec032b941977ba975c5b43c4318e859e
radubrehar <radu@jslog.com>
2014-10-18 08:05:19 +0300

    initial

---
+++ build.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    entry: './index.jsx',
+    output: {
+        filename: './bundle.js'
+    },
+    module: {
+        loaders: require('./loaders.config')
+    },
+    externals: {
+        'react': 'React'
+    },
+    resolve: {
+        // Allow to omit extensions when requiring these files
+        extensions: ['', '.js', '.jsx']
+    }
+}
